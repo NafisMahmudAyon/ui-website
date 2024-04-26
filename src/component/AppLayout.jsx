2dbfa3f9d458faf9b3753db22a786baa6e30ef42
NafisMahmudAyon <nafismahmudayon@gmail.com.com>
2024-04-26 09:06:54 +0600

    testing v2.1.9

--- src/component/AppLayout.jsx
+++ src/component/AppLayout.jsx
@@ -3,13 +3,13 @@ import { useLocation } from "react-router-dom";
 
 import Navbar from "./Layout/Navbar";
 import LeftSideBar from "./Layout/LeftSideBar";
+import RightSideBar from "./Layout/RightSideBar";
 import MiddleContent from "./Layout/MiddleContent";
 
-
-
-const AppLayout = ({ children }) => {
+const AppLayout = ({ children, RightSideBarContent }) => {
 	const location = useLocation();
-	const isVariationPage = location.pathname.includes("/variations");
+	const isPlaygroundPage = location.pathname === "/playground";
+	const isTestPage = location.pathname === "/test";
 
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
@@ -17,12 +17,11 @@ const AppLayout = ({ children }) => {
 		setIsSidebarOpen(!isSidebarOpen); // Toggle sidebar state
 	};
 	return (
-		<div className="bg-slate-100 font-sans scroll-smooth text-slate-700 dark:text-slate-200  dark:bg-darkBgColor  min-h-screen relative ">
+		<div className="bg-slate-100 font-sans text-slate-700 dark:text-slate-200  dark:bg-darkBgColor  min-h-screen relative ">
 			<Navbar
 				toggleSidebar={toggleSidebar}
 				className="fixed z-30 top-0 left-0 w-full"
 			/>
-			{/* <ScrollTop /> */}
 			<div
 				className={` ${
 					isSidebarOpen
@@ -30,21 +29,16 @@ const AppLayout = ({ children }) => {
 						: ""
 				} `}></div>
 			<div className="flex pt-[73px] flex-nowrap flex-col md:flex-row lg:flex-row relative h-screen overflow-y-scroll">
-				{/* //*mobile left sidebar  */}
 				<div
 					className={`fixed top-[73px] -left-[300px] w-[300px] transition-all duration-300 ease-in-out  pl-3 pr-5  border-r bg-darkBgColor text-darkTextColor z-50 overflow-y-scroll h-[calc(100vh_-_73px)] ${
 						isSidebarOpen ? "left-0" : ""
 					} `}>
 					<LeftSideBar />
 				</div>
-				<div className=" lg:block hidden lg:w-[20%] fixed top-[73px] pb-[100px] left-0 h-[100%]  border-r lg:overflow-y-scroll border-slate-950 dark:border-slate-300/60 pl-3 lg:pl-10 pr-5 lg:pr-10  ">
+				<div className="hidden lg:w-[20%]  lg:block border-r lg:overflow-y-scroll border-slate-950 dark:border-slate-300/60 pl-3 pr-5  ">
 					<LeftSideBar />
 				</div>
-				<div className="w-0 lg:w-[20%] "></div>
-				<div
-					className={`xs:w-full sm:w-full md:w-full lg:w-[80%] flex-1 ${
-						isVariationPage ? "md:px-0" : "md:px-5"
-					}  `}>
+				<div className="w-full lg:w-[80%] flex-1 md:pl-5 md:pr-5 ">
 					<MiddleContent>{children}</MiddleContent>
 					{/* {!isPlaygroundPage && !isTestPage && <RightSideBar />} */}
 				</div>
@@ -56,7 +50,4 @@ const AppLayout = ({ children }) => {
 	);
 };
 
-AppLayout.propTypes = {
-	children: React.ReactNode,
-};
 export default AppLayout;
--- src/component/Layout/MiddleContent.jsx
+++ src/component/Layout/MiddleContent.jsx
@@ -1,23 +1,11 @@
-
 import React from "react";
-import { useLocation } from "react-router-dom";
 
 const MiddleContent = ({ children }) => {
-	const location = useLocation();
-	const isVariationPage = location.pathname.includes("/variations");
 	return (
-		<div
-			className={`${
-				isVariationPage ? " " : "pl-5 pr-5 pb-8 "
-			} pt-2 max-h-[100%] scroll-smooth`}>
-			{/* <div className={`${isVariationPage ? " ":"pl-5 pr-5 pb-8 "} pt-2 max-h-[100%] overflow-scroll`}> */}
+		<div className="pl-5 pt-2 pb-8 pr-5 max-h-[100%] overflow-scroll">
 			{children}
 		</div>
 	);
-};
-
-MiddleContent.propTypes = {
-	children: React.ReactNode,
 };
 
 export default MiddleContent;
